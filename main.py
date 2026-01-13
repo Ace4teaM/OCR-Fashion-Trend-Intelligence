6188d672e2b2252136c8f83805916f57ef1c439d
Thomas Auguey <thomas.auguey@hotmail.com>
2026-01-13 15:34:56 +0100

    premiere requete avec reponse

--- main.py
+++ main.py
@@ -16,3 +16,35 @@ max_images = 3  # Commençons avec peu d'images
 
 # charge le token Huging face
 api_token = os.getenv("HUGGING_FACE_KEY")
+
+API_URL = "https://router.huggingface.co/hf-inference/models/sayeed99/segformer_b3_clothes"
+headers = {
+    "Authorization": f"Bearer {api_token}"
+    # Le "Content-Type" sera ajouté dynamiquement lors de l'envoi de l'image
+}
+
+# Lister les chemins des images à traiter
+# Assurez-vous d'avoir des images dans le dossier 'image_dir'!
+image_paths = os.listdir(image_dir)
+
+if not image_paths:
+    print(f"Aucune image trouvée dans '{image_dir}'. Veuillez y ajouter des images.")
+else:
+    print(f"{len(image_paths)} image(s) à traiter : {image_paths}")
+
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+        this_headers={"Content-Type": "image/jpeg", **headers}
+    elif filename.endswith(".png"):
+        this_headers={"Content-Type": "image/png", **headers}
+    else:
+        raise Exception("Format de fichier image inconnu : " + filename)
+    
+    response = requests.post(API_URL, headers=this_headers, data=data)
+    return response.json()
+
+output = query(f"{image_dir}/{image_paths[0]}")
+print("response",output)
